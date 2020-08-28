49ed5b7e43fe751c629c82a2c9cbcb84c88e91ff
Fernando-Scroppo <57918099+Fernando-Scroppo@users.noreply.github.com>
2020-08-28 19:07:50 -0300

    Juego + menu + personaje
    Ubicamos el personaje se mueve y salta.

--- main.py
+++ main.py
@@ -1,32 +1,135 @@
-
-# Importamos todos los modulos de pygame
 import pygame,sys
 from pygame.locals import *
 
-# Inicializamos todos los modulos de pygame
+#------------Colores------------------------
+Color =pygame.Color(70,80,150)
+Verde =pygame.Color(0, 255, 126)
+Gris = pygame.Color(165, 172, 171)
+Amarillo = pygame.Color(243, 254, 0)
+Azul = pygame.Color(0, 51, 254)
+Blanco = pygame.Color(255, 255, 255)
+Negro = pygame.Color(0,0,0)
+#------------inicio de ventana y configuracion del pygame------------------------
+
 pygame.init()
+ANCHO = 800 
+LARGO = 600
+ventana=pygame.display.set_mode((ANCHO,LARGO))
+pygame.display.set_caption("Primer jogo")
+gameExit = False
+reloj = pygame.time.Clock()
+click = False
 
-# Variable para las ventanas 
-ANCHO = 1280
-LARGO = 720
+#fuenteChica=pygame.font.Font("fuentes/E-BRAIN2.TTF",25)   #letra1 para el menu
+fuenteChica=pygame.font.Font("fuentes/EagleGTII.ttf",25)   #letra2 de marca ruedas para menu o titulo
+#fuenteChica=pygame.font.SysFont("comicsansms", 15)        #letra3 comun para menu
+fuenteMediana=pygame.font.SysFont("comicsansms", 30)
+fuenteGrande=pygame.font.SysFont("comicsansms", 80)
 
-ventana = pygame.display.set_mode((ANCHO, LARGO))
+#------------Tamaño de los botones------------------------
+tamBoton=[200,50]
+#------------Boton de jugar-------------------------------
+Boton=[50,100]
+ColorBotonJugar=[Gris,Azul]
+#------------Boton de Creditos-------------------------------
+BotonCreditos=[50,200]
+ColorBotonCreditos=[Gris,Amarillo]
+#------------Boton de Opciones-------------------------------
+BotonOpciones=[50,300]
+ColorBotonOpciones=[Gris,Azul]
+#------------Boton salir----------------------------------
+BotonSalir=[50,400]
+ColorBotonSalir=[Gris,Amarillo]
 
-#Colocar el titulo a la ventana
-pygame.display.set_caption("MegaScooter")
 
-#Declaramos las funciones
+def botones(ventana,color,pos,tam):
+    boton= pygame.draw.rect(ventana,color[1],(pos[0],pos[1],tam[0],tam[1]))
+    return boton
 
-def cargar_piso (piso1):
-    ventana.blit(piso1,(0,600))
-    ventana.blit(piso1,(1000,600)) 
-
+def objetotexto(texto,color,tamanio):
+    if tamanio=="pequenio":
+        textoSuperficie=fuenteChica.render(texto,True,color)
+    if tamanio=="mediano":
+        textoSuperficie=fuenteMediana.render(texto,True,color)
+    if tamanio=="grande":
+        textoSuperficie=fuenteGrande.render(texto,True,color)
     
+    return textoSuperficie, textoSuperficie.get_rect()
 
-# Bucle principal
-while True:
+def TextoDeBoton(msg,color,BotonX,BotonY,Ancho,Alto,tamanio="pequenio"):
+    textoSuperficie, textoRect = objetotexto(msg,color,tamanio)
+    textoRect.center = (BotonX + (Ancho/2),BotonY+(Alto/2))
+    ventana.blit(textoSuperficie,textoRect)
 
-    for event in pygame.event.get():
+
+
+
+# pygame.draw.line(ventana, Color, (60,80), (160,100),50) Dibujamos lineas
+""" pygame.draw.circle(ventana,(29,51,243),(80,90),250)
+pygame.draw.circle(ventana,(252,18,18),(150,310),250)
+
+pygame.draw.rect(ventana,(0,0,0),(150,70,130,45))
+pygame.draw.rect(ventana,(0,100,200),(155,77,120,32))
+
+pygame.draw.rect(ventana,(0,0,0),(150,140,130,45))
+pygame.draw.rect(ventana,(0,100,200),(155,147,120,32))
+
+pygame.draw.rect(ventana,(0,0,0),(150,210,130,45))
+pygame.draw.rect(ventana,(0,100,200),(155,217,120,32)) """
+
+
+""" while not gameExit:
+    for event in pygame.event.get()
+        if event.type == pygame.QUIT:
+            gameExit = True """
+
+fuente = pygame.font.SysFont(None, 20)
+pygame.font.get_fonts
+def mostrar_texto(text, font, color, surface, x, y):
+    textoObjeto = font.render(text,1,color)
+    textrect = textoObjeto.get_rect()
+    textrect.topleft = (x,y)
+    surface.blit(textoObjeto, textrect)
+
+
+def main_menu():
+    mostrar_texto('Menu Principal',fuente,(255,255,255),ventana,20,20)
+    pygame.mixer.music.load('Sonidos/dale-boca.mp3')
+    pygame.mixer.music.play(1)
+    while True:
+
+        fondo = pygame.image.load("Imagenes/bobo.jpg").convert()
+        ventana.blit(fondo, (0, 0))
+        
+        mx, my = pygame.mouse.get_pos()
+        opcion_1 = pygame.Rect(50,100,200,50)
+        opcion_2 = pygame.Rect(50,200,200,50)
+
+        
+
+        if opcion_1.collidepoint((mx,my)):
+            if click:
+                juego()
+        if opcion_2.collidepoint((mx,my)):
+            if click:
+                pygame.quit()
+                sys.exit()
+        #pygame.draw.rect(ventana,(255,0,0),opcion_1)
+        #pygame.draw.rect(ventana,(255,0,0),opcion_2)
+        botones(ventana,ColorBotonJugar,Boton,tamBoton)
+        botones(ventana,ColorBotonCreditos,BotonCreditos,tamBoton)
+        botones(ventana,ColorBotonOpciones,BotonOpciones,tamBoton)
+        botones(ventana,ColorBotonSalir,BotonSalir,tamBoton)
+        TextoDeBoton("Iniciar",Blanco,Boton[0],Boton[1],tamBoton[0],tamBoton[1])
+        TextoDeBoton("Salir",Negro,BotonCreditos[0],BotonCreditos[1],tamBoton[0],tamBoton[1])
+        TextoDeBoton("Creditos",Negro,BotonSalir[0],BotonSalir[1],tamBoton[0],tamBoton[1])
+        TextoDeBoton("Opciones",Blanco,BotonOpciones[0],BotonOpciones[1],tamBoton[0],tamBoton[1])
+
+        #mostrar_texto('Jugar',fuente,(255,255,255),ventana,115,120)
+        #mostrar_texto('Salir',fuente,(255,255,255),ventana,115,220)
+
+        click = False
+        for event in pygame.event.get():
             if event.type == QUIT:
                 pygame.quit()
                 sys.exit()
@@ -34,21 +137,32 @@ while True:
                 if event.key == K_ESCAPE:
                     pygame.quit()
                     sys.exit()
+            if event.type == MOUSEBUTTONDOWN:
+                if event.button == 1:
+                    click = True
+                
+        pygame.display.update()
+        reloj.tick(60)
 
-    # Cargar la imagen en una variable.
-    piso1 = pygame.image.load("Imagenes/piso1.jpg") 
-    moto_con_personaje = pygame.image.load("Imagenes/Personaje_con_moto.png")
+def juego():
+    seguir = True
+    while seguir:
+        ventana.fill((0,0,0))
+        mostrar_texto('Juego',fuente,(255,255,255),ventana,20,20)
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                pygame.quit()
+                sys.exit()
+            if event.type == KEYDOWN:
+                if event.key == K_ESCAPE:
+                    seguir = False
 
-    #Cargamos el piso en la ventana, con funciones.
-    cargar_piso (piso1)
+        pygame.display.update()
+        reloj.tick(60)
 
-    #Cargamos las imagenes del personaje
-    ventana.blit(moto_con_personaje,(0,500))
-    
-
-    #Actualiza la ventana
-    pygame.display.update()
+main_menu()
 
 
 
-    
+        
+
---
+++ main1.py
@@ -0,0 +1,193 @@
+# Importamos todos los modulos de pygame
+import pygame,sys
+from pygame.locals import *
+
+# Inicializamos todos los modulos de pygame
+pygame.init()
+
+# Variable para las ventanas 
+ANCHO = 800 
+LARGO = 600
+ventana = pygame.display.set_mode((ANCHO, LARGO))
+
+#Colocar el titulo a la ventana
+pygame.display.set_caption("MegaScooter")
+
+# class Piso():
+#     def __init__(self):
+#         self.x=0        #inicializacion en 0
+#         self.y=100
+
+# class Nivel():
+#     def __init__(self):
+#         self.velocidad=-6
+
+# velocidad=Nivel()
+# pisoNuevo=Piso()
+# def dibujarPiso():
+#     piso = pygame.image.load("Imagenes/piso1.jpg").convert()   #cargamos la imagen en variable fondo
+#     ventana.blit(piso, (pisoNuevo.x, 500))
+
+# def logicaPiso():
+    
+#     if(pisoNuevo.x<-800):  #-- -900 Si no llega a -800 resta 2px al costado la imagen
+#         pisoNuevo.x=800  # -- 100+ANCHO
+#     else:
+#         pisoNuevo.x+=velocidad.velocidad
+
+# En esta clase vamos a guardar las variables que utilizamos para hacer el fondo y el piso movil.
+class Variables_CargaImagenes():
+
+    def __init__(self):
+         self.xPiso=0       
+         self.xFondo=0
+
+def cargar_fondo():
+    #-----------------------------Fondo----------------------------------------------
+    fondo= pygame.image.load("Imagenes/fondo2.jpg").convert()
+    x_rel_Fondo= variables.xFondo % fondo.get_rect().width  # Hacemos el valor de "x" dividido "%" el ancho del fondo "fondo.get_rect().width"
+    ventana.blit(fondo, (x_rel_Fondo - fondo.get_rect().width, 0))
+
+    # Este if permite que el fondo se repita indefinidamente
+    if(x_rel_Fondo<ANCHO):
+        ventana.blit(fondo,(x_rel_Fondo,0))
+    variables.xFondo-=2
+    #----------------------------Fin Fondo-------------------------------------------
+
+def cargar_piso ():
+    #-------------------------------Piso-------------------------------------------
+    piso = pygame.image.load("Imagenes/piso1.jpg").convert()   #cargamos la imagen en variable piso
+    x_rel_Piso= variables.xPiso % piso.get_rect().width  #despues del % el comando obtiene el ancho
+    #de la foto siendo el divisor de xPiso devuelve el resto
+
+    ventana.blit(piso, (x_rel_Piso-piso.get_rect().width, 500))
+    if(x_rel_Piso<ANCHO):
+        ventana.blit(piso,(x_rel_Piso,500)) #Mostramos la imagen
+    variables.xPiso-=5   #Calcula la velocidad mientras el numero sea mas alto mas rapido ira el movimiento de la imagen
+    # xPiso es la cantidad de pixeles por segundo
+    #-----------------------------FinPiso-------------------------------------------
+
+
+def movimiento_moto ():
+
+    #-----------------------------Logica para mover la moto-------------------------------
+    #Variables globales
+    global cuentaPasos
+    global x
+
+    #Estos if anidados definen segun la tecla que se aprete las imagenes que se tiene que mostrar
+
+    #Contador de pasos
+    if cuentaPasos + 1 >= 4:   #>= 4 porque las fotos del movimiento de la moto son 4.
+        cuentaPasos = 0
+
+    #Movimiento hacia adelante acelerando
+    if acelera:   
+        ventana.blit(acelerando[cuentaPasos // 1], (int(px), int(py)))
+        cuentaPasos += 1
+
+    elif salto + 1 >= 1:
+        ventana.blit(salta,(int(px), int(py)))
+        cuentaPasos += 1
+
+    else:
+       ventana.blit(quieto,(int(px), int(py)))
+
+
+
+#Variables
+FPS =40 #creamos la variable para la cantidad de pixeles a la que queremos que se mueva el fondo
+reloj=pygame.time.Clock() # hacemos el reloj para determinar cada cuanto se actualiza
+variables = Variables_CargaImagenes() # En el objeto variables, guardamos las variables para mover el fondo y el piso
+
+#Imagenes para los movimientos del personaje
+
+quieto = pygame.image.load("Imagenes/Moto1.png")
+
+acelerando = [ pygame.image.load('Imagenes/Moto1.png'),
+            pygame.image.load('Imagenes/Moto2.png'),
+            pygame.image.load('Imagenes/Moto3.png'),
+            pygame.image.load('Imagenes/Moto4.png')
+          ]
+
+salta = pygame.image.load("Imagenes/Moto1.png")
+
+#----------------------------------------------------------
+
+#Variables del personaje:
+
+#Para poder ubicarlo en la pantalla
+x=0
+px = 0
+py = 390
+ancho = 40
+velocidad = 10
+
+#Variables para que pueda realizar el salto
+salto= False
+#Altura del salto
+cuentaSalto = 10
+
+#Variables de accion
+acelera = True     #Apretar Intro para que inicie a correr la moto.
+
+#Pasos
+cuentaPasos = 0
+
+
+
+
+
+#--------------------------------------------------------------------------------Comienza el bucle donde se ejecuta el juego-----------------------------------------
+
+# Bucle principal
+while True:
+    #logicaPiso()
+    #dibujarPiso()
+    for event in pygame.event.get():
+            if event.type == QUIT:
+                pygame.quit()
+                sys.exit()
+            if event.type == KEYDOWN:
+                if event.key == K_ESCAPE:
+                    pygame.quit()
+                    sys.exit()
+
+    #Opción tecla pulsada
+    keys = pygame.key.get_pressed()
+
+
+    #Tecla Enter - La moto arranca y comienza 
+
+    #Personaje quieto
+    if acelera != True :
+        acelera = False
+        cuentaPasos = 0    
+    
+    #Tecla SPACE - Salto
+    if not (salto):
+        if keys[pygame.K_SPACE]:
+            salto = True
+            cuentaPasos = 0
+    else:
+        if cuentaSalto >= -10:
+            py -= (cuentaSalto * abs(cuentaSalto)) * 0.5
+            cuentaSalto -= 1
+        else:
+            cuentaSalto = 10
+            salto = False
+
+
+    #Generamos el piso y el fondo movil.
+    
+    cargar_fondo()
+    cargar_piso()
+    movimiento_moto()
+
+    pygame.display.update()
+    reloj.tick(FPS)  #Definimos la cantidad de FPS a la que queremos que vaya el juego
+
+
+
+    
+    
